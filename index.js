2d9a5998b8851aa44f0f721c1b200afe8bcb4eea
Bruno Anhezini <bruno.anhezini@gmail.com>
2023-04-14 10:58:50 -0300

    slick slider

--- index.js
+++ index.js
@@ -31,3 +31,27 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1450);
+
+$(document).ready(function(){
+  $('.multiple-items').slick({
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
